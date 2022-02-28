85c7b6429ebd20e7c70dd11db6b310d09328402d
mubin_mhd <ariamubin79@gmail.com>
2022-02-28 11:39:15 +0700

    change cover

--- src/component/Home/CompHome.jsx
+++ src/component/Home/CompHome.jsx
@@ -1,5 +1,5 @@
 import React, { Component, useEffect } from "react";
-import quran from "../../img/quran.png";
+import quran from "../../img/sampulquran.png";
 import { Link } from "react-router-dom";
 import Aos from "aos";
 import "aos/dist/aos.css";
@@ -14,7 +14,7 @@ const CompHome = () => {
       data-aos="zoom-in"
     >
       <div className="default-font">
-        <img src={quran} alt="alquran logo" className="w-16 h-16 mx-auto" />
+        <img src={quran} alt="alquran logo" className="w-32 h-32 mx-auto" />
         <h3 className="text-4xl font-bold text-white mb-4 mt-2">Al Qur'an</h3>
         <Link to="/Chooise">
           <button className="bg-white text-gray-800 hover:bg-gray-100 font-semibold px-5 rounded-md text-xl">
